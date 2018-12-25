b4511e24bd5f4cf3b2d65e4eff06c2544834372a
xx961024 <44831085+xx961024@users.noreply.github.com>
2018-12-25 09:45:14 +0800

    Initial commit

---
+++ setup_cymysql.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm -rf CyMySQL
+rm -rf cymysql
+git clone https://github.com/xx961024/CyMySQL.git
+mv CyMySQL/cymysql ./
+rm -rf CyMySQL
---
+++ tests/libsodium/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ ! -d libsodium-1.0.1 ]; then
+    wget https://github.com/xx961024/SSR/blob/master/file/libsodium/libsodium-1.0.1.tar.gz || exit 1
+    tar xf libsodium-1.0.1.tar.gz || exit 1
+fi
+pushd libsodium-1.0.1
+./configure && make -j2 && make install || exit 1
+sudo ldconfig
+popd
